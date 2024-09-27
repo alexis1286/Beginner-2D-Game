08322a3fe34acc8be1a8a19d83bbcd09ef8be162
Alexis Lovitz <97564748+alexis1286@users.noreply.github.com>
2024-09-26 23:04:42 -0400

    added new script

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator anim;
+    private PlayerMovement playerMovement;
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -77,9 +77,6 @@ public class PlayerMovement : MonoBehaviour
            }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-    }
-
     private bool isGrounded(){
 
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size, 0 , Vector2.down, 0.1f,groundLayer);
